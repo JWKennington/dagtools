2e38fbe43dac3dc449a04494e7639aba7de073fc
JWKennington <jameswkennington@gmail.com>
2020-01-20 16:31:34 -0600

    add package setup file

---
+++ dagtools/__init__.py
@@ -0,0 +1,7 @@
+__MAJOR__ = 0
+__MINOR__ = 0
+__MICRO__ = 1
+__VERSION__ = (__MAJOR__, __MINOR__, __MICRO__)
+__version__ = '.'.join(str(n) for n in __VERSION__)
+__github_url__ = 'http://github.com/JWKennington/dagtools'
+
